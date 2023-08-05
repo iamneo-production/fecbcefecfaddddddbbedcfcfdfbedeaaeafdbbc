aa7a26b8337584b7cbd5464eb7dc591bcfdf7b06
Iamneo <techadmin@iamneo.ai>
2023-08-05 10:26:36 +0000

    Backup Commit

--- reactapp/src/tests/Button.test.js
+++ reactapp/src/tests/Button.test.js
@@ -2,19 +2,21 @@ import React from "react";
 import Button from "../components/UI/Button/Button";
 import { shallow, mount } from "enzyme";
 
-describe("Testing Button Component", () => {
-  // render the button without any error
+describe('Testing Button Component', () => {
 
-  test("testcase8", () => {
-    const wrapper = shallow(<Button />);
-    expect(wrapper.instance()).toBeDefined();
-  });
+    // render the button without any error
+    
+    test('testcase6', () => {
+        const wrapper = shallow(<Button />);
+        expect(wrapper.instance()).toBeDefined();
+    })
 
-  // displays the correct text inside the component
+    // displays the correct text inside the component
 
-  test("testcase9", () => {
-    const TestingInput = "Button Works";
-    const wrapper = shallow(<Button>{TestingInput}</Button>);
-    expect(wrapper.props().children).toBe(TestingInput);
-  });
-});
+    test('testcase7', () => {
+        const TestingInput = 'Button Works'
+    const wrapper = shallow(<Button>{TestingInput}</Button>)
+        expect(wrapper.text()).toBe(TestingInput)
+    })
+
+})
